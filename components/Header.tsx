c286a0d7eb06aabd9dc8fc4bd61319e950d26a1b
Damon Malkiewicz <46790806+dcmalk@users.noreply.github.com>
2024-03-12 12:02:56 -0400

    Update color scheme, add logos, and extend screen sizes

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,11 +12,11 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            <div className="mr-3">
+            <div className="mr-1">
               <Logo />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
+              <div className="hidden text-xl font-semibold xs:block sm:text-2xl">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
